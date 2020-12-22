3430b4236a2cd6b9a8b0ad22897c7a38a4594458
https://learn.co/DenaNeuman <denaweiss5@gmail.com>
2020-12-22 15:03:05 -0600

    commit tree 29d77d706c33333434751c4a432903fbded9e975

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Your code here
+function mapToNegativize(sourceArray){
+    let x = []
+    for(let i = 0; i < sourceArray.length; i++){
+        x.push(sourceArray[i]*(-1))
+    }
+return x
+}
+
+function mapToNoChange(sourceArray){
+    return sourceArray
+}
+
+function mapToDouble(sourceArray){
+    let x = []
+    for(let i = 0; i<sourceArray.length; i++){
+        x.push(sourceArray[i]*(2))
+    }
+    return x
+}
+
+function mapToSquare(sourceArray){
+    let x = []
+    for(let i = 0; i<sourceArray.length; i++){
+        x.push(sourceArray[i]**(2))
+    }
+    return x
+}
+
+function reduceToTotal(src, start=0){
+    let total = start
+    for (let i=0; i<src.length; i++){
+        total += src[i]
+    }
+return total
+}
+
+function reduceToAllTrue(src){
+    for (let i=0; i<src.length; i++){
+        if (!src[i]) return false
+    }
+    return true
+}
+
+function reduceToAnyTrue(src){
+    for (let i=0; i<src.length; i++){
+        if (src[i]) return true
+    }
+return false
+}
